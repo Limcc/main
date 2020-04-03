56129414992f76c0ec98912a387fbca48716ded6
limc <melissa0223@naver.com>
2020-04-03 20:15:54 +0900

    tutorial

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,39 @@
 import 'package:flutter/material.dart';
-import 'package:haha/pages/initial.dart';
+import 'package:carousel_pro/carousel_pro.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(SliderApp());
 
-class MyApp extends StatelessWidget {
+class SliderApp extends StatelessWidget {
+  final PageController ctrl = PageController();
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        primarySwatch: Colors.amber,
-      ),
-      home: InitialPage(),
+      title: 'Carousel App Demo',
+      home: SliderHome(),
+          );
+        }
+      }
+      
+  class SliderHome extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ImageCarouse1(),
+          );
+        }
+      }
+      
+  class ImageCarouse1 extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Carousel(
+      images: [
+        new NetworkImage('https://post-phinf.pstatic.net/MjAxODAxMjZfMjE4/MDAxNTE2OTUwMTA4NDQz.8H38FcR90M8nUx6MZwIEfEZI12zkxlbJy7UU7mgPL_cg.z_IrPLg-7iqQJCgI0w1ZFAu0tqDtqTDhe8dHaiXFaZIg.JPEG/iphone-wallpaper13.jpg?type=w1200'),
+        new AssetImage('assets/voice.jpg'),
+        new NetworkImage('assets/add.jpg'),
+        new NetworkImage('assets/command.jpg'),
+      ],
     );
   }
 }
